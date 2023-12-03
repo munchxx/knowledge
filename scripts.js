c44d50e029916fe5c1bc0f708ae9840e673e8ab5
munch <davexjerry@gmail.com>
2023-12-04 06:40:31 +1300

    Added basic function and styling

--- scripts.js
+++ scripts.js
@@ -1,25 +1,30 @@
 $(document).ready(function () {
-    const articleListContainer = $("#articleList");
-
-    // Fetch and display articles dynamically
-    fetchArticles().then(articles => {
-        articles.forEach(article => {
-            // Create a clone of the article template
-            const articleTemplate = $("<iframe>").attr("src", `articles/${article.folder}/index.html`);
-
-            // Append the article to the homepage
-            articleListContainer.append(articleTemplate.contents());
-        });
-    });
-
-    // Function to fetch article list (replace with actual logic)
-    async function fetchArticles() {
-        // Use fetch or any other method to get the list of articles
-        // Return an array of article objects with title, date, tags, etc.
-        return [
-            { folder: 'article1', title: 'Article Title 1', date: 'January 1, 2023', tags: ['Tag1', 'Tag2'] },
-            { folder: 'article2', title: 'Article Title 2', date: 'January 5, 2023', tags: ['Tag3', 'Tag4'] },
-            // Add more articles as needed
-        ];
-    }
+    loadArticles();
 });
+
+function loadArticles() {
+    fetchArticles()
+        .done(displayArticles)
+        .fail(handleError);
+}
+
+function fetchArticles() {
+    // Fetch a list of articles (you may need to adjust the path)
+    return $.getJSON("articles-list.json");
+}
+
+function displayArticles(articles) {
+    // Sort articles by date (assuming articles have a "date" property)
+    articles.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+    // Display articles in the article-list container
+    let articleListContainer = $("#article-list");
+    $.each(articles, function (index, article) {
+        let articleElement = $("<div>").html(`<a href="${article.path}"><h2>${article.title}</h2><p>${article.date}</p></a>`);
+        articleListContainer.append(articleElement);
+    });
+}
+
+function handleError(jqXHR, textStatus, errorThrown) {
+    console.error("Error loading articles:", errorThrown);
+}
